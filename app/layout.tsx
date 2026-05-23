250c632515753e64dd36a6595c5864c25a346c35
Laxman Chandra Roy <laxmanroy.me@gmail.com>
2026-05-23 19:10:09 +0600

    added header, footer, route and some folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,13 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 export default function RootLayout({ children }) {
   return (
     <html>
       <body>
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   )
