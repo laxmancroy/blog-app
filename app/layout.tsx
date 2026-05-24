3b2aec1bee56fea52b878ab024ed1ef749f1813a
Laxman Chandra Roy <laxmanroy.me@gmail.com>
2026-05-24 18:39:42 +0600

    some minor problem solve

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './ui/Header';
+import Footer from './ui/Footer';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
