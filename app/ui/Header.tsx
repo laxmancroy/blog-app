4fe0a0ad684615e399dc31298a008e786c7ba731
Laxman Chandra Roy <laxmanroy.me@gmail.com>
2026-05-25 19:44:13 +0600

    added some style in header components

--- app/ui/Header.tsx
+++ app/ui/Header.tsx
@@ -1,15 +1,21 @@
+const navBar = [
+    { name: 'Home', href: '#home' },
+    { name: 'About', href: '#about' },
+    { name: 'Sign in', href: '#signin' },
+    { name: 'Sign up', href: '#signup' }
+];
+
 export default function Header() {
     return (
-        <header id="header" className="bg-blue-400 text-white flex justify-around py-2 ">
+        <header id="header" className="bg-gray-600 text-white flex justify-around py-2 ">
             <div>Blog</div>
             <nav>
-                <ul className="flex justify-center *:bg-amber-400 *:px-2 *:py-1 *:mx-2 *:rounded-md *:hover:cursor-pointer">
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Sign in</li>
-                    <li>Sign up</li>
-                    <li>Contact</li>
-
+                <ul className="flex justify-center *:border *:border-gray-600 *:px-2 *:py-1 *:mx-2 *:rounded-md *:hover:cursor-pointer *:hover:border-white">
+                    {
+                        navBar.map(link => (
+                            <a key={link.href} href={link.href}>{link.name}</a>
+                        ))
+                    }
                 </ul>
             </nav>
         </header>
