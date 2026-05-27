398e3967d09d6ba64aea404997d093b2ae34185d
Laxman Chandra Roy <laxmanroy.me@gmail.com>
2026-05-27 18:54:06 +0600

    added signup page boilerplate

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -3,9 +3,9 @@ import Link from 'next/link';
 export default function Signin() {
     return (
         <div className="w-full text-center my-6">
-            <div className="bg-foreground inline-block text-white rounded-md px-8 py-5">
+            <div className="bg-foreground inline-block text-white rounded-md px-8 py-5 border-[1px] border-border">
                 <h2 className="text-3xl mb-5">Sign In</h2>
-                <form>
+                <form className="my-9">
                     <label htmlFor="email">Email</label>
                     <br />
                     <input type="text" placeholder="Email" />
@@ -13,6 +13,8 @@ export default function Signin() {
                     <label htmlFor="password">Password</label>
                     <br />
                     <input type="password" placeholder="Password" />
+                    <br />
+                    <input type="submit" value="Submit" className="bg-blue-600 text-white w-full py-3 rounded-md" />
                 </form>
                 <p className="block mt-8">If you are not a member, please <Link href="/signup">sign up</Link> first.</p>
             </div>
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -2,9 +2,26 @@ import Link from 'next/link';
 
 export default function Signup() {
     return (
-        <div>
-            <h2>Sign Up</h2>
-            <p>If you have a account, go to <Link href="/signin">Login</Link> page</p>
+        <div className="w-full text-center">
+            <div className="inline-block bg-foreground border-[1px] border-border my-8 px-8 py-5">
+                <h2>Sign Up</h2>
+                <form>
+                    <label htmlFor="name">Name</label>
+                    <br />
+                    <input type="text" />
+                    <br />
+                    <label htmlFor="email">Email</label>
+                    <br />
+                    <input type="text" />
+                    <br />
+                    <label htmlFor="password">Password</label>
+                    <br />
+                    <input type="text" />
+                    <br />
+                    <input type="submit" value="Submit" className="bg-blue-500 text-white w-full rounded-md py-3" />
+                </form>
+                <p className="mt-6">If you have a account, go to <Link href="/signin">Login</Link> page</p>
+            </div>
         </div>
     )
 }
