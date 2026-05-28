e38fff3cc96dcc651d3e2d5cb48a8d8978e67222
Laxman Chandra Roy <laxmanroy.me@gmail.com>
2026-05-28 17:42:05 +0600

    header mobile menu boilerplate added

--- app/ui/Header.tsx
+++ app/ui/Header.tsx
@@ -1,15 +1,21 @@
+'use client';
+
+import { useState } from 'react';
+
 const navBar = [
-    { name: 'Home', href: '#home' },
-    { name: 'About', href: '#about' },
+    { name: 'Home', href: '/#home' },
+    { name: 'About', href: '/#about' },
     { name: 'Sign in', href: '/signin' },
     { name: 'Sign up', href: '/signup' }
 ];
 
 export default function Header() {
+    const [menuOpen, setMenuOpen] = useState(false);
     return (
         <header id="header" className="bg-foreground text-white flex justify-around py-2 border-b-[1px] border-border ">
             <div className="text-2xl">Blog</div>
-            <nav>
+            {/* Desktop Navigation Bar */}
+            <nav className="hidden sm:block">
                 <ul className="flex justify-center *:border *:border-gray-600 *:px-2 *:py-1 *:mx-2 *:rounded-md *:hover:cursor-pointer *:hover:border-white">
                     {
                         navBar.map(link => (
@@ -18,6 +24,22 @@ export default function Header() {
                     }
                 </ul>
             </nav>
+
+            {/* Mobile Navigation Bar */}
+            <button className="sm:hidden" onClick={() => (setMenuOpen(!menuOpen))}>menu</button>
+            {menuOpen &&
+                <nav className="w-full h-screen bg-yellow-500 absolute top-0 left-0 overflow-hidden">
+                    <ul>
+                        {
+                            navBar.map(link => (
+                                <a key={link.href} href={link.href}>{link.name}</a>
+                            ))
+                        }
+                    </ul>
+                    <button onClick={() => (setMenuOpen(false))}>close</button>
+                </nav>
+
+            }
         </header>
     )
 }
