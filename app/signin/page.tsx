d45ee8c192591254cd4965a3caa18cb5e7c7b021
Laxman Chandra Roy <laxmanroy.me@gmail.com>
2026-05-28 17:12:50 +0600

    modified signin and signup page

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -2,21 +2,17 @@ import Link from 'next/link';
 
 export default function Signin() {
     return (
-        <div className="w-full text-center my-6">
-            <div className="bg-foreground inline-block text-white rounded-md px-8 py-5 border-[1px] border-border">
+        <div className="bg-red-600 container mx-auto">
+            <div className="bg-foreground inline-block text-white rounded-md px-8 py-5 border-[1px] border-border my-10">
                 <h2 className="text-3xl mb-5">Sign In</h2>
-                <form className="my-9">
-                    <label htmlFor="email">Email</label>
+                <form className="my-9 *:focus:outline-0 *:w-full">
+                    <input type="text" placeholder="Email" className="border-[1px] border-border h-9 rounded-md" />
                     <br />
-                    <input type="text" placeholder="Email" />
+                    <input type="password" placeholder="Password" className="border-[1px] border-border h-9 mt-5 rounded-md" />
                     <br />
-                    <label htmlFor="password">Password</label>
-                    <br />
-                    <input type="password" placeholder="Password" />
-                    <br />
-                    <input type="submit" value="Submit" className="bg-blue-600 text-white w-full py-3 rounded-md" />
+                    <input type="submit" value="Submit" className="bg-blue-600 text-white w-full py-3 rounded-md mt-5" />
                 </form>
-                <p className="block mt-8">If you are not a member, please <Link href="/signup">sign up</Link> first.</p>
+                <p className="block mt-8">If you are not a member, please <Link href="/signup" className="text-blue-600">sign up</Link> first.</p>
             </div>
         </div>
     )
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -2,25 +2,20 @@ import Link from 'next/link';
 
 export default function Signup() {
     return (
-        <div className="w-full text-center">
-            <div className="inline-block bg-foreground border-[1px] border-border my-8 px-8 py-5">
+        <div className="bg-red-600 container mx-auto">
+            <div className="inline-block bg-foreground border-[1px] border-border my-10 px-8 py-5 rounded-md">
                 <h2>Sign Up</h2>
-                <form>
-                    <label htmlFor="name">Name</label>
+                <form className="*:focus:outline-0 *:h-9 *:my-2 *:w-full *:border-[1px] *:border-border *:rounded-md">
                     <br />
-                    <input type="text" />
+                    <input type="text" placeholder="Full Name" />
                     <br />
-                    <label htmlFor="email">Email</label>
+                    <input type="email" placeholder="Email" />
                     <br />
-                    <input type="text" />
-                    <br />
-                    <label htmlFor="password">Password</label>
-                    <br />
-                    <input type="text" />
+                    <input type="password" placeholder="Password" />
                     <br />
                     <input type="submit" value="Submit" className="bg-blue-500 text-white w-full rounded-md py-3" />
                 </form>
-                <p className="mt-6">If you have a account, go to <Link href="/signin">Login</Link> page</p>
+                <p className="mt-6">If you have a account, go to <Link href="/signin" className="text-blue-600">login</Link> page.</p>
             </div>
         </div>
     )
