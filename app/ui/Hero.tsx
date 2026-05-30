a73797fa3ff5c0cdcc768b6be1f6f4f68181f0e0
Laxman Chandra Roy <laxmanroy.me@gmail.com>
2026-05-30 16:31:17 +0600

    Hero section boilerplate added

--- app/ui/Hero.tsx
+++ app/ui/Hero.tsx
@@ -1,5 +1,15 @@
+import Image from 'next/image';
+
 export default function Hero() {
     return (
-        <div>This is hero section</div>
+        <div>
+            <div className="flex justify-center my-6">
+                <button>prev</button>
+                <Image src="/thumbnails/rainyCafeMemories-noLogo.jpg" alt="" width={500
+                } height={300} />
+                <button>next</button>
+            </div>
+            <div className="text-center">.   .   .</div>
+        </div>
     )
 }
