c2b5c9b0f5407c7540113716968e659a386277e1
Laxman Chandra Roy <laxmanroy.me@gmail.com>
2026-07-22 19:01:29 +0600

    remove a unwanted component from home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import Hero from './ui/Hero';
-import ContactForm from './ui/ContactForm';
 
 export default function page() {
   return (
     <>
       <Hero />
       <div>This is homepage</div>
-      <ContactForm />
     </>
   )
 }
